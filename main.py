347a719d5e1b2e12572b232e9d7713f3450b7682
Sherry Ummen <sherry.umm@outlook.com>
2019-10-21 00:33:18 +0300

    correlation chart

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-
 def parse_args(args):
     parser = argparse.ArgumentParser(
         description="Options:")
     parser.add_argument('-l','--lc', dest='violin_chart',help='Plot Violin chart (Distribution between X and Avg user count)',action='store_true')
-    parser.add_argument('-c','--cor', dest='correlation',help='Plot Correlation between Day and Avg user count')
-    parser.add_argument('-v','--verbose',dest='verbose', help='Train model with Verbose')
-    parser.add_argument('-p','--predict', dest='predict', help='Print the prediction')
+    parser.add_argument('-c','--cor', dest='correlation_chart',help='Plot Correlation between Day and Avg user count',action='store_true')
+    parser.add_argument('-v','--verbose',dest='verbose', help='Train model with Verbose',action='store_true')
+    parser.add_argument('-p','--predict', dest='predict', help='Print the prediction',action='store_true')
     parser.add_argument('-f','--file', dest='file', help='csv file path', default='./test_data/users_elisa.csv')
     return parser.parse_args(args)
 
@@ -24,6 +23,12 @@ def plot_violin(data):
     vp = ax.violinplot(data.iloc[:,4:].values)
     plt.show()
 
+def plot_correlation_chart(data):
+    new_data = data.drop(data.columns[[0,2,3,4,5,6,7,8,9,10,11,12]], axis=1)
+    plt.clf()
+    new_data.groupby('Day').mean().plot(kind='bar', legend=None)
+    plt.show()
+
 
 def main(args):
     args = parse_args(args)
@@ -33,9 +38,10 @@ def main(args):
     # Violin chart
     if(args.violin_chart):
         plot_violin(data)
-
-
-
+    # Correlation chart
+    if(args.correlation_chart):
+        plot_correlation_chart(data)
+    
 
 if __name__ == "__main__":
     main(sys.argv[1:])
